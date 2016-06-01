b4247fc6d052832570f27d453e129a0f1a093852
jp ventura <JPeg@Jean-pauls-MacBook-Pro.local>
2016-06-01 14:49:49 -0400

    Sample functions first save

---
+++ sample_functions.py
@@ -0,0 +1,11 @@
+'''this will be a collection of our first set of functions '''
+
+# Our First function!!
+def max_of_three(arg1,arg2,arg3):
+    '''this function takes in three arguments and returns the largest value'''
+    if arg1>arg2 and arg1>arg3:
+        return arg1
+    elif arg2>arg3 and arg2>arg1:
+            return arg2
+    else:
+        return arg3
